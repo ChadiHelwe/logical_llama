cf44b906c5b55706290f7d794aaf5165854ca578
ChadiHelwe <chadi.helwe@gmail.com>
2023-09-08 19:30:58 +0200

    update code and .gitignore

--- generate_instuction_based_datasets/amr2fol.py
+++ generate_instuction_based_datasets/amr2fol.py
@@ -1,9 +1,17 @@
+import json
 import os
 from collections.abc import Generator
 from typing import Tuple
 
 from amr_logic_converter import AmrLogicConverter
 from tqdm import tqdm
+
+INSTRUCTION_TEXT_TO_FOL = "Generate the following text to FOL."
+INSTRUCTION_FOL_TO_TEXT = "Generate the following FOL to text."
+INSTRUCTION_TEXT_TO_AMR = "Generate the following text to AMR."
+INSTRUCTION_AMR_TO_TEXT = "Generate the following AMR to text."
+INSTRUCTION_AMR_TO_FOL = "Generate the following AMR to FOL."
+INSTRUCTION_FOL_TO_AMR = "Generate the following FOL to AMR."
 
 
 class AmrToFolConverter:
@@ -48,16 +56,17 @@ class AmrToFolConverter:
         return sentences, amrs
 
     def process_directory(
-        self, directory_path
+        self, directory_paths
     ) -> Generator[Tuple[str, str], None, None]:
         """Process all files in the given directory and yield the corresponding sentences and AMRs."""
         sentences = []
         amrs = []
-        files = [
-            os.path.join(directory_path, f)
-            for f in os.listdir(directory_path)
-            if f.endswith(".txt")
-        ]
+        files = []
+
+        for directory_path in directory_paths:
+            for f in os.listdir(directory_path):
+                if f.endswith(".txt"):
+                    files.append(os.path.join(directory_path, f))
 
         for file_path in files:
             s, a = self.extract_amr_and_sentences(file_path)
@@ -70,7 +79,7 @@ class AmrToFolConverter:
         error_count = 0
         for i, amr in enumerate(tqdm(amrs)):
             try:
-                yield sentences[i], self.convert(amr)
+                yield sentences[i], amr, self.convert(amr)
             except Exception as e:
                 error_count += 1
 
@@ -78,10 +87,27 @@ class AmrToFolConverter:
         print(f"Errors: {error_count} ({error_count / len(sentences) * 100:.2f} %)")
 
 
+def generate_instruction_instances(sentence, amr, fol):
+    """Generate all instruction instances for the given sentence, AMR and FOL."""
+    return [
+        {"instruction": INSTRUCTION_TEXT_TO_FOL, "input":sentence, "output": fol},
+        {"instruction": INSTRUCTION_FOL_TO_TEXT, "input": fol, "output": sentence},
+        {"instruction": INSTRUCTION_TEXT_TO_AMR, "input": sentence, "output": amr},
+        {"instruction": INSTRUCTION_AMR_TO_TEXT, "input": amr, "output": sentence},
+        {"instruction": INSTRUCTION_AMR_TO_FOL, "input": amr, "output": fol},
+        {"instruction": INSTRUCTION_FOL_TO_AMR, "input": fol, "output": amr},
+    ]
+
+
 if __name__ == "__main__":
     converter = AmrToFolConverter()
-    root_dir = "datasets/amr_annotation_3.0/data/amrs/unsplit/"
-    for s, f in converter.process_directory(root_dir):
-        print(s)
-        print(f)
-        print()
+    root_dir = ["datasets/amr_annotation_3.0/data/amrs/split/training", "datasets/amr_annotation_3.0/data/amrs/split/dev"]
+
+    with open("logicmancer_dataset.json", "w") as out:
+        out.write("[\n")
+        for s, a, f in converter.process_directory(root_dir):
+            dict_instruction_instances = generate_instruction_instances(s, a, f)
+            for instruction_instance in dict_instruction_instances:
+                out.write(json.dumps(instruction_instance))
+                out.write(",\n")
+        out.write("]\n")
